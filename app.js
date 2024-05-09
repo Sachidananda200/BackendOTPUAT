19653fb1af951c63199dffb1ec18b9afbd5972f0
Sachidananda200 <109515559+Sachidananda200@users.noreply.github.com>
2024-05-09 14:31:11 +0530

    9thMayFInalChange

--- app.js
+++ app.js
@@ -116,6 +116,7 @@ app.post('/sms', async (req, res) => {
         const otp = otpMatch ? otpMatch[0] : null;
 
         const Messege_time = moment(message_time).format('YYYY/MM/DD HH:mm:ss');
+        console.log(sender, Messege_time, otp, user_mobile, message,host);
 
         // Get connection pool based on selected database
         let pool;
